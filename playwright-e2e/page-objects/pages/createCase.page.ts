ce21db724eed26b33d057896a9c3917fe9854104
SuganyaHMCTS <Suganya.jagadeesan@hmcts.net>
2025-09-29 09:51:15 +0100

    Add two defendants in create case

--- playwright-e2e/page-objects/pages/createCase.page.ts
+++ playwright-e2e/page-objects/pages/createCase.page.ts
@@ -1,5 +1,6 @@
 import { expect, Locator } from "@playwright/test";
 import { Base } from "../base";
+import { link } from "fs";
 
 class CreateCasePage extends Base {
   createCaseLink: Locator;
@@ -13,6 +14,18 @@ class CreateCasePage extends Base {
   submitCreateBtn: Locator;
   ddCaseProsecutedBy: Locator;
   caseTitle: Locator;
+  addDefButton: Locator;
+  dOBDay: Locator;
+  dOBMonth: Locator;
+  dOBYear: Locator;
+  surName: Locator;
+  firstName: Locator;
+  roleSelect: Locator;
+  defUrn: Locator;
+  defOne: Locator;
+  defTwo: Locator;
+  addBtn: Locator;
+  FpgBtn: Locator;
 
 constructor(page) {
     super(page);
@@ -27,16 +40,28 @@ constructor(page) {
     this.frontPgDesc =  page.locator('#Description_ifr');
     this.submitCreateBtn =  page.locator("//input[@value='Create']");
     this.caseTitle = page.locator('.heading-medium');
+    this.addDefButton = page.getByRole("link", {name: "Add Defendant"});
+    this.dOBDay = page.locator('#DobDay');
+    this.dOBMonth = page.locator('#DobMonth');
+    this.dOBYear = page.locator('#DobYear');
+    this.surName = page.locator('#Surname');
+    this.firstName = page.locator('#FirstName');
+    this.roleSelect = page.locator('[name="roleSelector"]')
+    this.defOne = page.locator('[name="Defendant One  - 01.01.70"]')
+    this.defTwo = page.locator('[name="Defendant Two  - 01.02.70"]')
+    this.defUrn = page.locator('#Urn')
+    this.addBtn = page.locator('#add-p')
+    this.FpgBtn = page.getByRole("text", {name:"Back to Front Page"})
+
 }
 
 async createCaseRandom(caseName: string, caseUrn: string){
-    const randomNumber = Math.floor(Math.random() * 10000) + 1;
+    const randomNumber = Math.floor(Math.random() * 10000) + 100;
     const caseRandom = caseName+randomNumber;
     const urnRandom = caseUrn+randomNumber;
     console.log(caseRandom);
     return {caseRandom, urnRandom};
 }
-
 
 async selectRandomOptionByLabel(): Promise<string> {
     const labels = await this.ddCaseProsecutedBy.locator('option').allTextContents();
@@ -62,8 +87,20 @@ async createNewCase(caseName: string, caseUrn: string) {
     await this.hearingDateMonth.selectOption({ label: monthName.toString() });
     await this.hearingDateYear.selectOption({ label: year.toString() });
     await this.submitCreateBtn.click();
-}}
+    return urnRandom;
+}
 
+async addDefendants(surName: string, dOBMonth: string){
+    await this.addDefButton.click();
+    await this.firstName.fill("Defendant");
+    await this.surName.fill(surName.toString());
+    await this.dOBDay.selectOption("1");
+    await this.dOBMonth.selectOption(dOBMonth.toString());
+    await this.dOBYear.selectOption("1990");
+    await this.addBtn.click();
+}
+
+}
 export default CreateCasePage;
 
 
--- playwright-e2e/tests/createCase.spec.ts
+++ playwright-e2e/tests/createCase.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "../fixtures";
 import { UserCredentials, config, invalidUsers } from "../utils";
 
-test.describe("Create New Test Case in CCDCS", () => {
+test.describe("Create New Case in CCDCS", () => {
 test.beforeEach(async ({ homePage, loginPage }) => {
     await homePage.open();
     await homePage.navigation.navigateTo("ViewCaseListLink");
@@ -18,6 +18,15 @@ test("Create New Case", async ({
     await createCasePage.createCaseLink.click();
     await createCasePage.createNewCase('TestCase','TestURN');
     await expect (createCasePage.caseTitle).toBeVisible();
+    
+    const defDetails = [
+    { surName: 'One', dobMonth: 'January' },
+    { surName: 'Two', dobMonth: 'February' },
+    ]
+      for (const defDetail of defDetails) {
+      await createCasePage.addDefendants(defDetail.surName, defDetail.dobMonth);
+      await expect (createCasePage.caseTitle).toBeVisible();
+      }
   });
   
 });
