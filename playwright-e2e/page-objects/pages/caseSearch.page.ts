67e85d4ad48fe777e87940a19d147583d6d2b490
CourtneySuhr-Solirius <courtney.suhr@solirius.com>
2025-10-06 09:48:07 +0100

    renaming of caseList to caseSearch

--- playwright-e2e/page-objects/pages/caseSearch.page.ts
+++ playwright-e2e/page-objects/pages/caseSearch.page.ts
@@ -1,7 +1,7 @@
 import { Locator } from "@playwright/test";
 import { Base } from "../base";
 
-class CaseListPage extends Base {
+class CaseSearchPage extends Base {
   textField: Locator;
   viewCaseListLink: Locator;
   locationField: Locator;
@@ -79,4 +79,4 @@ class CaseListPage extends Base {
   }
 }
 
-export default CaseListPage;
+export default CaseSearchPage;
