acca19846aeee81c59181c5be9c6a6321fad11c0
SuganyaHMCTS <Suganya.jagadeesan@hmcts.net>
2025-12-08 11:13:46 +0000

    updated lint errors

--- playwright-e2e/page-objects/pages/index.page.ts
+++ playwright-e2e/page-objects/pages/index.page.ts
@@ -67,7 +67,7 @@ async indexSectionTitle(row: number): Promise<string> {
         
         return trimmedTitle;
 
-    } catch (_) {
+    } catch {
         throw new Error(`Failed to retrieve section title for row ${row}`);
     }
 }
@@ -117,7 +117,7 @@ await expect
         { timeout: 15000 }
     )
     .toBe(true);
-}catch (_) {
+}catch {
     throw new Error(`Failed to retrieve the Index table contents`);
 }}
 
