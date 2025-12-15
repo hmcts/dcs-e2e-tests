c8bd679b99c4cb209a8bd0bfa0e0b81cef9100c7
Courtney_Suhr <courtney.suhr@solirius.com>
2025-12-15 15:30:32 +0000

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -63,16 +63,16 @@ export default defineConfig({
     //   },
     //   dependencies: ["setup"],
     // },
-    // {
-    //   name: "firefox",
-    //   use: {
-    //     ...ProjectsConfig.firefox.use,
-    //     //Use below line when running docker
-    //     channel: undefined,
-    //     storageState: "./playwright-e2e/.sessions/trainer01.json",
-    //   },
-    //   dependencies: ["setup"],
-    // },
+    {
+      name: "firefox",
+      use: {
+        ...ProjectsConfig.firefox.use,
+        //Use below line when running docker
+        channel: undefined,
+        storageState: "./playwright-e2e/.sessions/trainer01.json",
+      },
+      dependencies: ["setup"],
+    },
     {
       name: "firefox-notes-serial",
       testMatch: /notes\.spec\.ts/,
