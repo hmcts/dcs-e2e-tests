30eb0152a686857c628d3593c9e0102137866911
CourtneySuhr-Solirius <courtney.suhr@solirius.com>
2025-12-17 09:46:30 +0000

    testing serial run with soft fail

--- playwright-e2e/tests/notes.spec.ts
+++ playwright-e2e/tests/notes.spec.ts
@@ -154,10 +154,14 @@ for (const user of Object.values(config.users).filter(
       });
 
       if (currentUserIssues.length > 0) {
-        throw new Error(
-          `User ${user.group} experienced issues:\n${currentUserIssues.join(
-            "\n"
-          )}`
+        // throw new Error(
+        //   `User ${user.group} experienced issues:\n${currentUserIssues.join(
+        //     "\n"
+        //   )}`
+        // );
+        test.fail(
+          true,
+          `Issues found for ${user.group}:\n${currentUserIssues.join("\n")}`
         );
       }
     });
@@ -207,7 +211,7 @@ test.describe("Notes Permissions & Access", () => {
   for (const user of Object.values(config.users).filter(
     (user) => !excludedGroups.includes(user.group)
   )) {
-    test(`Verify access to Notes for: ${user.group}`, async ({
+    test(`Verify access to Notes for: ${user.group} @notes`, async ({
       loginPage,
       homePage,
       caseSearchPage,
@@ -261,10 +265,14 @@ test.describe("Notes Permissions & Access", () => {
 
       // Fail the test if any issues were found
       if (currentUserIssues.length > 0) {
-        throw new Error(
-          `User ${
-            user.group
-          } has missing/unexpected Notes:\n${currentUserIssues.join("\n")}`
+        // throw new Error(
+        //   `User ${
+        //     user.group
+        //   } has missing/unexpected Notes:\n${currentUserIssues.join("\n")}`
+        // );
+        test.fail(
+          true,
+          `Issues found for ${user.group}:\n${currentUserIssues.join("\n")}`
         );
       }
     });
