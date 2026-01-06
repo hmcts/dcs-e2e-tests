ab741ad2b1c12e26d3c62aac1f79904e38f74972
CourtneySuhr-Solirius <courtney.suhr@solirius.com>
2026-01-06 14:09:38 +0000

    validation of registered user table

--- playwright-e2e/page-objects/pages/userSettings.page.ts
+++ playwright-e2e/page-objects/pages/userSettings.page.ts
@@ -1,8 +1,9 @@
 import { Locator } from "@playwright/test";
 import { Base } from "../base";
+import { expect } from "../../fixtures";
 
-class UserSettingsPage extends Base { 
-  userSettingsHeading : Locator;
+class UserSettingsPage extends Base {
+  userSettingsHeading: Locator;
   searchText: Locator;
   applyFilter: Locator;
   changeIsVerifiedUser: Locator;
@@ -10,30 +11,53 @@ class UserSettingsPage extends Base {
   approvedUserFlag: Locator;
   deniedUserFlag: Locator;
   userResultsTitle: Locator;
-      
-constructor(page) {
-    super(page);
-    this.userSettingsHeading = page.locator('div#content h2')
-    this.searchText = page.locator('#searchText')
-    this.applyFilter = page.getByRole('link', { name: 'Apply Filter' })
-    this.changeIsVerifiedUser = page.locator("xpath=(//a[contains(text(),'Change')])[5]")
-    this.verifiedUserFlag = page.locator("xpath=//*[@id=\"personListDiv\"]/table/tbody/tr[2]/td[12]")
-    this.approvedUserFlag = page.locator("xpath=//*[@id=\"personListDiv\"]/table/tbody/tr[2]/td[7]")
-    this.deniedUserFlag = page.locator("xpath=//*[@id=\"personListDiv\"]/table/tbody/tr[2]/td[14]")
-    this.userResultsTitle = page.locator('td').filter({ hasText: 'Results:' }).first()
-}
+  userResultsContainer: Locator;
+  userResultsTable: Locator;
 
-async searchUser(userName: string){
+  constructor(page) {
+    super(page);
+    this.userSettingsHeading = page.locator("div#content h2");
+    this.searchText = page.locator("#searchText");
+    this.applyFilter = page.getByRole("link", { name: "Apply Filter" });
+    this.userResultsContainer = page.locator("#personListDiv");
+    this.userResultsTable = page.locator(".formTable-zebra");
+    this.changeIsVerifiedUser = page.locator(
+      "xpath=(//a[contains(text(),'Change')])[5]"
+    );
+    this.verifiedUserFlag = page.locator(
+      'xpath=//*[@id="personListDiv"]/table/tbody/tr[2]/td[12]'
+    );
+    this.approvedUserFlag = page.locator(
+      'xpath=//*[@id="personListDiv"]/table/tbody/tr[2]/td[7]'
+    );
+    this.deniedUserFlag = page.locator(
+      'xpath=//*[@id="personListDiv"]/table/tbody/tr[2]/td[14]'
+    );
+    this.userResultsTitle = page
+      .locator("td")
+      .filter({ hasText: "Results:" })
+      .first();
+  }
+
+  async usersTableLoad() {
+    const loaders = this.userResultsContainer.locator(
+      'img[alt="Please wait ..."]'
+    );
+    await expect(loaders).toHaveCount(0, { timeout: 60_000 });
+  }
+
+  async searchUser(userName: string) {
     await this.searchText.clear();
     await this.searchText.fill(userName);
     await this.applyFilter.click();
-}
+    await this.usersTableLoad();
+    await expect(this.userResultsTable).toBeVisible({ timeout: 30000 });
+  }
 
-async updateVerifyUserFlag(){
+  async updateVerifyUserFlag() {
     await this.changeIsVerifiedUser.click();
     const verifiedFlag = this.verifiedUserFlag.textContent;
     return verifiedFlag;
-}
-
+  }
 }
 export default UserSettingsPage;
